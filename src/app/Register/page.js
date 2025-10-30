38ab5307dbfecad9857f1089131544c0f4e6bc5b
E-commerce-React-app <09iqrasiddique.com>
2025-10-30 23:02:56 +0500

    deleted

--- src/app/Register/page.js
+++ src/app/Register/page.js
@@ -1,11 +1,7 @@
 
 import ProtectedRoute from '@/components/Auth/ProtectedRoute'
 import Register from '@/components/Registration/Register'
-// import Signup from '@/components/Registration/Signup'
 
-// import Signup from '@/components/Registration/SignUp'
-
-// import Signup from '@/components/Registration/Signup'
 import React from 'react'
 
  
@@ -16,7 +12,7 @@ import React from 'react'
      
     
 
-{/* <Signup/>   */}
+
 <Register/>
  
   </>
